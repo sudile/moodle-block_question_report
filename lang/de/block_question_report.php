72323f43c51b51a558cafe402ad8511b85382cbb
Vincent Schneider <vincent.schneider@gmx.net>
2023-01-16 12:26:09 +0100

    added lang strings for de

---
+++ lang/de/block_question_report.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings
+ *
+ * @package    block
+ * @subpackage question_report
+ * @copyright  2022 sudile GbR (http://www.sudile.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Vincent Schneider <vincent.schneider@sudile.com>
+ */
+
+$string['pluginname'] = 'Fragebericht';
+$string['privacy:metadata'] = 'Das Plugin "Question report block" speichert keine persönlichen Daten.';
+$string['reports'] = 'Berichte ansehen';
+$string['back'] = 'Zurück zur Auswahl.';
+$string['group'] = 'Gruppe';
+$string['user'] = 'Benutzer';
+
+$string['download'] = 'Herunterladen';
+$string['default_below_50'] = 'nicht ausreichend';
+$string['default_below_57'] = 'ausreichend';
+$string['default_below_72'] = 'zufriedenstellend';
+$string['default_below_87'] = 'gut';
+$string['default_below_100'] = 'sehr gut';
+
+$string['table_name'] = 'Name';
+$string['table_percentage'] = 'Prozent';
+$string['table_feedback'] = 'Feedback';
+$string['table_progress'] = 'Leistung';
+$string['table_avgprogress'] = 'Gruppenleistung';
+
+$string['config_content'] = 'Beschreibung';
+$string['config_data_visibility'] = 'Sichtbarkeitseinstellungen';
+
+$string['data_visibility_all'] = 'Zeige alles';
+$string['data_visibility_diagram'] = 'Nur Diagramm';
+$string['data_visibility_table'] = 'Nur Tabelle';
